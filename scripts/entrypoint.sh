1a6c101baaab96f26a9a3f9ab191251388460912
cfgaspar10 <carlesandrogaspar@gmail.com>
2021-03-12 00:36:43 -0300

    comitando projeto

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e # exit if errors happen anywhere
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+uwsgi --socket :8000 --master --enable-threads --module mysite.wsgi
---
+++ src/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mbpdegaspar/dockerinzando/rhsispen_test3/src/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
